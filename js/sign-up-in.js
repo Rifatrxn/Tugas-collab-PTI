7bb3d4382c967db50a38b20692407d91abe76bc8
Rifatrxn <rifatrexana12345@gmail.com>
2025-11-21 01:45:23 +0700

    Sign In Page

--- js/sign-up-in.js
+++ js/sign-up-in.js
@@ -2,17 +2,17 @@ const password = document.getElementById("password")
 const togglePass = document.getElementById("togglePass");
 
 togglePass.addEventListener("click", () =>{
-    // if(password.type === "password"){
-    //     password.type = "text";
-    //     togglePass.textContent = "img"
-    // }
-    // else{
-    //     password.type = "password";
-    //     togglePass.textContent = "hoe"
-    // }
-    const isPassword = password.type === "password";
+    if(password.type === "password"){
+        password.type = "text";
+        togglePass.src = "../assets/assets-sign-up-in/hide.png"
+    }
+    else{
+        password.type = "password";
+        togglePass.src = "../assets/assets-sign-up-in/view.png"
+    }
+    // const isPassword = password.type === "password";
 
-    password.type = isPassword ? "text":"password";
+    // password.type = isPassword ? "text":"password";
 
-    togglePass.src = isPassword ? "../assets/assets-sign-up-in/hide.png" : "../assets/assets-sign-up-in/view.png";
+    // togglePass.src = isPassword ? "../assets/assets-sign-up-in/hide.png" : "../assets/assets-sign-up-in/view.png";
 });
